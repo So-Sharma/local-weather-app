cc23a6dbee1ac90d8e69a37bd638f028e049c0fe
Soumya S <soumyas@live.com>
2019-05-19 10:16:31 -0700

    Service and Transform

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  appId: '37241575d7560403a73b54538de6e00f',
+  baseUrl: 'http://'
 };
 
 /*
